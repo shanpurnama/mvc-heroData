212b350406030bd90f5cec0517bb9d27b8cbeb7a
shanpurnama <shsute03@gmail.com>
2021-08-19 19:31:20 +0700

    add condition delete

--- index.js
+++ index.js
@@ -19,14 +19,15 @@ if (command[0] === 'read') {
             console.log(err)
         } else {
             var dataJson = JSON.parse(data)
-            var newDataHero = {
-                hero_id: dataJson.hero.length,
-                hero_name: command[1],
-                hero_avatar: command[2],
-                hero_role: command[3],
-                hero_specially: command[4]
+            for (var i = 0; i < dataJson.hero.length; i++) {
+                var newDataHero = {
+                    hero_id: dataJson.hero[i].hero_id + 1,
+                    hero_name: command[1],
+                    hero_avatar: command[2],
+                    hero_role: command[3],
+                    hero_specially: command[4]
+                }
             }
-
             dataJson.hero.push(newDataHero)
             var dataStringify = JSON.stringify(dataJson)
             fs.writeFile('./db/heroData.json', dataStringify, 'utf-8', function(err) {
@@ -34,6 +35,22 @@ if (command[0] === 'read') {
                     console.log(err)
                 }
                 console.log('successfuly add new data')
+            })
+        }
+    })
+} else if (command[0] === 'delete') {
+    fs.readFile('./db/heroData.json', 'utf-8', function(err, data) {
+        if (err) {
+            console.log(err, 'error nih bos')
+        } else {
+            var dataJsonParse = JSON.parse(data)
+            dataJsonParse.hero.splice(10, 1)
+            var dataStringify = JSON.stringify(dataJsonParse)
+            fs.writeFile('./db/heroData1.json', dataStringify, 'utf-8', function(err) {
+                if (err) {
+                    console.log(err, 'err')
+                }
+                console.log('berhasil boss')
             })
         }
     })
